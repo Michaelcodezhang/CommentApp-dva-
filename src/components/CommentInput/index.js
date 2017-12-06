4b2b8281b0c23d9376f730dc3c4235cbda2a5cee
张景浩 <zhangjinghao@hrsoft.net>
2017-12-06 22:03:50 +0800

    dva重构的CommentApp

---
+++ src/components/CommentInput/index.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react'
+import {connect} from 'dva'
+import {Input, Button} from 'antd'
+import './index.less'
+
+const {TextArea} = Input
+
+class CommentInput extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      content: ''
+    }
+    this.handleContentChange = this.handleContentChange.bind(this)
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+
+  }
+
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handleContentChange(e) {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
+  handleSubmit() {
+    this.props.dispatch({
+      type: 'app/addComment',
+      comment: {
+        username: this.state.username,
+        content: this.state.content
+      }
+    })
+    const temp = [...this.props.app.comments, {
+      username: this.state.username,
+      content: this.state.content
+    }]
+    const c = JSON.stringify(temp)
+    localStorage.setItem('comments', c)
+  }
+
+  render() {
+    return (
+      <div className='comment-input-container'>
+        <div>
+          <div className='username-font'>用户名：</div>
+          <div><Input className='username-container' type='text' onChange={this.handleUsernameChange} /></div>
+        </div>
+        <div>
+          <div className='content-font'>评论内容：</div>
+          <div><TextArea className='content-container' name="" id="" cols="20" rows="10"
+                         onChange={this.handleContentChange} /></div>
+        </div>
+        <div className="comment-input-container-button">
+          <Button type='primary' style={{'width': '100%'}} onClick={this.handleSubmit}>发布</Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(({app}) => ({app}))(CommentInput)
